df4b54b2906afeac5018b06848143d641eae75d7
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-08-19 18:08:00 +0100

    api working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [searchValue, setSearchValue] = useState("bygone");
+
+  const getMeaning = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.dictionaryapi.dev/api/v2/entries/en/${searchValue}`
+      );
+      const data = await response.data;
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getMeaning();
+  }, [searchValue]);
+
   return <>Hello</>;
 }
 
